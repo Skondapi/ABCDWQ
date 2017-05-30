915cec6f2b32efcc8369e7341084fdbdc3b73e8a
Pramod Kumar <pramodkumar.para@mroads.com>
2017-05-30 23:17:17 +0530

    Added few more items in home page

--- Shop-Layout/HTML/Angular/myApp.js
+++ Shop-Layout/HTML/Angular/myApp.js
@@ -46,10 +46,30 @@ var list = [
         rating:4,
         price:599,
         description:'Park Hyatt Hyderabad'
+    },{
+        imageURL:'images/shop/dialogueInTheDark.jpg',
+        rating:4.5,
+        price:599,
+        description:'Dialogue in the dark'
+    },
+    {
+        imageURL:'images/shop/menchies.jpg',
+        rating:4,
+        price:899,
+        description:'Menchies Frozen Yogurt'
+    },{
+        imageURL:'images/shop/abs.jpg',
+        rating:3.5,
+        price:599,
+        description:'Absolute bareque'
+    },
+    {
+        imageURL:'images/shop/hypercitymall.jpg',
+        rating:4.5,
+        price:399,
+        description:'Hyper City Mall'
     }
-
 ];
-
 
 var parallaxList = [
     {
